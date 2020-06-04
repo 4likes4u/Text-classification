6c98bd4a91cde345c65b7e54cf9daa2b072328aa
4likes4u <63773512+4likes4u@users.noreply.github.com>
2020-06-04 21:51:02 +0800

    Update bert.py

--- bert.py
+++ bert.py
@@ -79,42 +79,6 @@ class BertClassifier(nn.Module):
         res = self.fc(output)
         return res
 
-class LSTM(nn.Module): 
-    def __init__(self):
-        super(LSTM, self).__init__()
-        self.embedding = nn.Embedding(5895, 100) #5895 个词，每个词的嵌入是100维
-        self.lstm = nn.LSTM(input_size=100, hidden_size=100, num_layers=1, batch_first=True)#定义LSTM，LSTM的输入是(batch_size, seq_len, embedding_size)
-        #注意LSTM的输出有两个，我们只要第一个
-#         self.dropout = nn.Dropout(0.2) # 后期我们会详述嘛是dropout
-        self.fc1 = nn.Linear(100, 300)
-        self.fc2 = nn.Linear(300, 3)
-
-    def forward(self, x):
-        x = self.embedding(x)
-        x,_ = self.lstm(x)
-        x = x.mean(dim=1) #为了节省空间，依旧采用比较简单的mean 操作
-#         x = self.dropout(x)
-        x = self.fc1(x)
-        x = x.relu()
-        x = self.fc2(x)
-        return x
-
-class DAN(nn.Module): 
-    def __init__(self):
-        super(DAN, self).__init__()
-        self.embedding = nn.Embedding(5895, 100) #5895 个词，每个词的嵌入是100维
-        self.fc1 = nn.Linear(100, 300)
-        self.fc2 = nn.Linear(300, 3)
-
-    def forward(self, x):
-        x = self.embedding(x)
-        x = x.mean(dim=1)
-#         x = self.dropout(x)
-        x = self.fc1(x)
-        x = x.relu()
-        x = self.fc2(x)
-        return x
-#实例化
 model = BertClassifier().cuda(9)
 
 
